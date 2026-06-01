bc1046a536166093f2f72a4064d0434dcd36422a
Lucas <prilosac@gmail.com>
2026-06-01 16:41:03 -0700

    including DiffViewer in my default nvim config, very useful

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1424,6 +1424,9 @@ require("lazy").setup({
 			})
 		end,
 	},
+	{
+		"sindrets/diffview.nvim",
+	},
 	require("plugins.ai"),
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
